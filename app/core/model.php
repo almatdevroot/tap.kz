b4d95e3f84f4fd18fff2fd1b7a1e2d1aebe97f68
Almat <almatdevroot@gmail.com>
2017-03-09 18:03:53 +0600

    commit 3

--- app/core/model.php
+++ app/core/model.php
@@ -6,11 +6,11 @@ class Model {
     }
 
     protected function find($params) {
-
+        return $this->db->pdo->query("SELECT * FROM `".$this->t_n."` ".$params.";")->fetch(PDO::FETCH_ASSOC);
     }
 
     protected function findId($id) {
-
+        return $this->db->pdo->query("SELECT * FROM `".$this->t_n."` WHERE `id` = ".$this->db->pdo->quote($id).";")->fetch(PDO::FETCH_ASSOC);
     }
 
     protected function create($params) {
